b7d17cf3073aa10d0add1c65a53af2db995eb194
T Z N <tuzunalican@gmail.com>
2022-04-12 14:59:57 +0200

    Drill_Done

--- Fourth_Chapter/Drill.cpp
+++ Fourth_Chapter/Drill.cpp
@@ -7,7 +7,7 @@
 #include <numeric>
 double mm_converter(double n, std::string unit);
 
-int main () {
+int main (void) {
     double first = 0;
     double second = 0;
     double kopo = 0;
@@ -19,26 +19,41 @@ int main () {
 
     std::cout << "Please enter one number and unit\n";
 
+    while (std::cin >> first >> unit){
 
-    // Keep track of the sum of values entered (as well as the smallest and the largest) and the number of values entered.
-    // When the loop ends, print the smallest, the largest, the number of values, and the sum of values.
-    // Note that to keep the sum, you have to decide on a unit to use for that sum; use meters.
-    while (std::cin >> kopo >> unit){
-        if(mm_converter(kopo,unit) == 0 || unit.empty())
-        {
-            double first =mm_converter(kopo, unit);
             double div_val_f = std::abs(first - second);
             double div_val_s = std::abs(second - first);
             double largest = 0;
             if (first == '|' || second == '|') {
-            break;
+                std::cout << "Terminated\n";
+                break;
             }
             else {
+                if(unit == "cm")
+                {
+                    first =first*10;
+                }
+                else if(unit == "m"){
+                    first = first*1000;
+                }
+                else if(unit == "in"){
+                    first = first*10*2.54;
+                }
+                else if(unit == "ft"){
+                    first = first*12*2.54*10;
+                }
+                else
+                {
+                    std::cout << "Sorry its not acceptable! \n";
+                    break;
+                }
                 values.push_back(first);
+                std::sort(values.begin(),values.end());
                 if (first > second) {
                     ++number_of_values;
-                    largest = first;//
+                    largest = first;
                     std::cout << "the larger value so far  : " << largest << "mm" << "\n";
+                    second = first;
                     if (div_val_f <= div_lim && second != 0 || div_val_s <= div_lim && second != 0) {
                         std::cout << "the numbers are almost equal\n";
                     }
@@ -48,6 +63,7 @@ int main () {
                     if (largest <= second)
                         largest = second;
                     std::cout << "the larger value so far  : " << largest << "mm"<< "\n";
+                    first = second;
                     if (div_val_f <= div_lim && second != 0 || div_val_s <= div_lim && second != 0) {
                         std::cout << "the numbers are almost equal\n";
                     }
@@ -56,43 +72,16 @@ int main () {
                     std::cout << "the numbers are equal\n";
                 }
             }
-            second = first;
         }
-        else
-        {
-            std::cout << "Sorry its not acceptable!";
-            break;
-        }
+        std::cout << "Maximum element: " << *max_element(values.begin(), values.end()) << "mm\n";
+        std::cout << "Minimum element: " << *min_element(values.begin(), values.end()) << "mm\n";
+        std::cout << "Number of values: " << number_of_values << "mm\n";
+        std::cout << "All numbers added: " <<  std::accumulate(values.begin(),values.end(),0.0) << "mm\n";
     }
-    std::cout << "Maximum element" << *max_element(values.begin(), values.end()) << "\n";
-    std::cout << "Minimum element" << *min_element(values.begin(), values.end()) << "\n";
-    std::cout << "Number of values" << number_of_values << "\n";
-    std::cout << "All numbers added" <<  std::accumulate(values.begin(),values.end(),0.0);
-}
 
 
-double mm_converter(double n, std::string unit)
-{
-    double converted_value = 0;
-    if(unit == "cm")
-    {
-        converted_value =n*10;
-    }
-    else if(unit == "m"){
-        converted_value = n*100;
-    }
-    else if(unit == "in"){
-        converted_value = n*10*2.54;
-    }
-    else if(unit == "ft"){
-        converted_value = n*12*2.54*10;
-    }
-    else
-    {
-        converted_value = 0;
-    }
-    return converted_value;
-}
+
+
 
 
 
